c9def6f35d9821fdc44b5c28ed6ac19762a8def6
Dzaakyy <dzakynurwahid46@gmail.com>
2025-05-10 08:52:13 +0700

    perbaikan sedikit

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -32,7 +32,7 @@ const userController = {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: false,
             maxAge: 24 * 60 * 60 * 1000, 
             sameSite: 'none',
         })
